17612c2dd9c23a878b7d208f3b464c0b027567ca
HungNguyen <www.thuphan@gmail.com>
2025-10-06 08:21:23 +0700

    Set ExecutionTimeLimit to 0

--- cre_task.ps1
+++ cre_task.ps1
@@ -1,7 +1,7 @@
 $WHCCName = "Windows Hello Camera Control"
 $WHCCPrincipal = New-ScheduledTaskPrincipal -RunLevel Highest -UserId "SYSTEM" -LogonType ServiceAccount
 $WHCCAction = New-ScheduledTaskAction -Execute "powershell" -Argument "-NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy Bypass -File .\whcc.ps1" -WorkingDirectory $pwd.Path
-$WHCCSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries
+$WHCCSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries -ExecutionTimeLimit 0
 $WHCCTrigger = New-ScheduledTaskTrigger -AtStartup
 Register-ScheduledTask -TaskName $WHCCName -Principal $WHCCPrincipal -Action $WHCCAction -Description "Windows Hello Control" -Settings $WHCCSettings -Trigger $WHCCTrigger
 Start-ScheduledTask -TaskName $WHCCName
