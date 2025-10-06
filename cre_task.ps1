eea6512a2a74d1237cf7c5eca48d3f667a82edb1
HungNguyen <www.thuphan@gmail.com>
2025-10-06 08:36:57 +0700

    Fixed task description

--- cre_task.ps1
+++ cre_task.ps1
@@ -3,5 +3,5 @@ $WHCCPrincipal = New-ScheduledTaskPrincipal -RunLevel Highest -UserId "SYSTEM" -
 $WHCCAction = New-ScheduledTaskAction -Execute "powershell" -Argument "-NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy Bypass -File .\whcc.ps1" -WorkingDirectory $pwd.Path
 $WHCCSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries -ExecutionTimeLimit "PT0S"
 $WHCCTrigger = New-ScheduledTaskTrigger -AtStartup
-Register-ScheduledTask -TaskName $WHCCName -Principal $WHCCPrincipal -Action $WHCCAction -Description "Windows Hello Control" -Settings $WHCCSettings -Trigger $WHCCTrigger
+Register-ScheduledTask -TaskName $WHCCName -Principal $WHCCPrincipal -Action $WHCCAction -Description $WHCCName -Settings $WHCCSettings -Trigger $WHCCTrigger
 Start-ScheduledTask -TaskName $WHCCName
