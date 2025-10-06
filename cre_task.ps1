8b9424a31e8cc13ad15fa76bb38decf7eb631e07
HungNguyen <www.thuphan@gmail.com>
2025-10-06 09:53:26 +0700

    Added the ability to stop the script and remove startup task

--- cre_task.ps1
+++ cre_task.ps1
@@ -1,10 +1,16 @@
+param (
+    [switch]$remove = $false
+)
+
 $WHCCName = "Windows Hello Camera Control"
+Stop-ScheduledTask -TaskName $WHCCName -ErrorAction SilentlyContinue
+Unregister-ScheduledTask -TaskName $WHCCName -Confirm:$false -ErrorAction SilentlyContinue
+
+if ($remove) { exit 0 }
+
 $WHCCPrincipal = New-ScheduledTaskPrincipal -RunLevel Highest -UserId "SYSTEM" -LogonType ServiceAccount
 $WHCCAction = New-ScheduledTaskAction -Execute "powershell" -Argument "-NoProfile -NonInteractive -WindowStyle Hidden -ExecutionPolicy Bypass -File .\whcc.ps1" -WorkingDirectory $pwd.Path
 $WHCCSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries -ExecutionTimeLimit 0 -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1)
 $WHCCTrigger = New-ScheduledTaskTrigger -AtStartup
-
-Stop-ScheduledTask -TaskName $WHCCName -ErrorAction SilentlyContinue
-Unregister-ScheduledTask -TaskName $WHCCName -Confirm:$false -ErrorAction SilentlyContinue
 Register-ScheduledTask -TaskName $WHCCName -Principal $WHCCPrincipal -Action $WHCCAction -Description $WHCCName -Settings $WHCCSettings -Trigger $WHCCTrigger
 Start-ScheduledTask -TaskName $WHCCName
